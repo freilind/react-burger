2c70e9158f5c7bec523f4e7ebc81b60c8cde268f
Freilin Manzano <freilin.manzano@externos.bci.cl>
2020-09-19 13:36:31 -0300

    first commit burger app

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux/Aux'
+import classes from './Layout.css'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDraw from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render(){
+        return(
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDraw 
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}> 
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default layout;
