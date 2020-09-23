7d7ec581645a5f15ec1ef1d23e9faf67b2287d90
Freilin Manzano <freilin.manzano@externos.bci.cl>
2020-09-23 18:21:59 -0300

    add orders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
 
@@ -12,7 +15,11 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          { this.state.show ? <BurgerBuilder /> : null }
+          <Switch>
+            <Route path='/checkout' component={Checkout} />
+            <Route path='/orders' component={Orders} />
+            <Route path='/' exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
